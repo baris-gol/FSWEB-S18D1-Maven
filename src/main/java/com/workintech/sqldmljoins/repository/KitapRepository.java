0f8c5cc7f79071e577dff55fe231a1a0547d43d4
baris <bariss.gol@gmail.com>
2025-04-22 20:48:00 +0300

    tamamlandı

--- src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
+++ src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
@@ -8,15 +8,12 @@ import java.util.List;
 
 public interface KitapRepository extends JpaRepository<Kitap, Long> {
 
-    //Dram ve Hikaye türündeki kitapları listeleyin. JOIN kullanmadan yapın.
-    String QUESTION_1 = "select k.* from kitap k, tur t where k. turno=t.turno and t.ad in ('Hikaye' , 'Dram');";
-    @Query(value = QUESTION_1, nativeQuery = true)
-    List<Kitap> findBooks();
+    // Dram ve Hikaye türündeki kitapları listeleyin (JOIN kullanmadan)
+    @Query("SELECT k FROM Kitap k WHERE k.tur.ad IN ('Dram', 'Hikaye')")
+    List<Kitap> findBooksByType();
 
-
-    String QUESTION_10 = " select avg (k.puan) from kitap k; ";
+    // Kitapların puanlarının ortalamasını al
+    String QUESTION_10 = "select avg(k.puan) from kitap k;";
     @Query(value = QUESTION_10, nativeQuery = true)
     Double findAvgPointOfBooks();
-
-
 }
